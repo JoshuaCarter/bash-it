62e167def49a7398cfcbf1350054b6a97dd143c4
jcarter <jcarter.dev@pm.me>
2022-10-03 11:24:03 +1100

    add netstat

--- custom/aliases/jc.aliases.bash
+++ custom/aliases/jc.aliases.bash
@@ -5,6 +5,7 @@ alias ntop="sudo nethogs -v 0 -d .333"
 alias ctop="htop"
 alias gtop="watch -n 0.1 nvidia-smi"
 alias mon="bashtop"
+alias ports="sudo netstat -ntlp"
 
 # process
 alias stalk="ps -e | grep "
@@ -16,13 +17,13 @@ alias push="git push origin HEAD "
 alias pull="git pull origin HEAD "
 alias discard="git restore ."
 function revert() {
-    git fetch origin $(git rev-parse --abbrev-ref HEAD)
-    git reset FETCH_HEAD --hard
+  git fetch origin $(git rev-parse --abbrev-ref HEAD)
+  git reset FETCH_HEAD --hard
 }
 function squash() {
-    git reset --soft HEAD~$1 &&
+  git reset --soft HEAD~$1 &&
     git commit --edit -m"$(git log --format=%B --reverse HEAD..HEAD@{1})"
 }
 function git-patch() {
-  git diff --no-prefix "${1}" > "${2}"
+  git diff --no-prefix "${1}" >"${2}"
 }
